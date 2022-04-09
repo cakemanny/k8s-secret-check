0d8be7a8e26c1c3a67711342de6294d816c03e5d
cakemanny <goldingd89@gmail.com>
2022-04-09 03:42:02 +0200

    Add option to send webhooks to Slack
    This also stops the container quitting after each watch call.

--- README.md
+++ README.md
@@ -21,3 +21,35 @@ linting and code review processes. Mistakes happen. Things go down.
 **k8s-secret-check** watches your secret and logs if they are invalid.
 
 
+## Config
+
+| Env var               | Description |
+|-----------------------|-------------|
+| `SLACK_WEBHOOK_URL`   | The Slack webhook url for sending notifications to |
+
+## Installation
+
+```shell
+cat > kustomization.yaml <<EOF
+namespace: secretiveapps
+bases:
+  - https://github.com/cakemanny/k8s-secret-check/bases/main?ref=master
+patches:
+- target:
+    kind: Deployment
+    name: k8s-secret-check
+  patch: |-
+    apiVersion: apps/v1
+    kind: Deployment
+    metadata:
+      name: k8s-secret-check
+    spec:
+      template:
+        spec:
+          containers:
+          - name: k8s-secret-check
+            env:
+            - name: SLACK_WEBHOOK_URL
+              value: https://hooks.slack.com/services/XXXXXXXXX/YYYYYYYYYYY/zzzzzzzzzzzzzzzzzzzzzzzz
+EOF
+```
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 use futures::{StreamExt, TryStreamExt};
-use kube_client::api::{Api, ResourceExt, ListParams, WatchEvent};
-use kube_client::Client;
 use k8s_openapi::api::core::v1::Secret;
-use yaml_rust::{YamlLoader};
+use kube_client::Client;
+use kube_client::api::{Api, ResourceExt, ListParams, WatchEvent};
 use serde::Serialize;
 use serde_json::Value;
+use std::env;
+use yaml_rust::{YamlLoader};
+use reqwest;
 
 
 #[derive(Serialize)]
 pub enum LogSeverity {
-    DEBUG, INFO, WARNING, ERROR
+    DEBUG, INFO, NOTICE, WARNING, ERROR
 }
 
 #[derive(Serialize)]
@@ -26,12 +28,44 @@ fn log(severity: LogSeverity, message: String) {
 }
 
 fn log_debug(msg: String) { log(LogSeverity::DEBUG, msg) }
-fn log_info(msg: String) { log(LogSeverity::INFO, msg) }
+fn log_notice(msg: String) { log(LogSeverity::NOTICE, msg) }
 fn log_err(msg: String) { log(LogSeverity::ERROR, msg) }
+
+#[derive(Serialize)]
+pub struct WebhookBody {
+    text: String,
+}
+
+async fn notify(msg: String) {
+
+    if let Ok(webhook_url) = env::var("SLACK_WEBHOOK_URL") {
+        let body = WebhookBody{text: msg};
+        let client = reqwest::Client::new();
+        let res_or_err = client.post(webhook_url)
+            .json(&body)
+            .send()
+            .await;
+
+        match res_or_err {
+            Err(err) => {
+                log_err(format!("{}", err))
+            },
+            Ok(res) => {
+                log_debug(format!("Webhook fired: {}", res.status()));
+            }
+        }
+    }
+}
 
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    log_notice("k8s-secret-check started".to_string());
+
+    if env::var("SLACK_WEBHOOK_URL").is_err() {
+        log_notice("SLACK_WEBHOOK_URL is not configured.".to_string());
+    }
+
     // Read the environment to find config for kube client.
     // Note that this tries an in-cluster configuration first,
     // then falls back on a kubeconfig file.
@@ -39,28 +73,38 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let secrets: Api<Secret> = Api::default_namespaced(client);
 
-    let mut stream  = secrets.watch(&ListParams::default(), "0").await?.boxed();
+    let mut resource_version = "0".to_owned();
+    loop {
+        let mut stream  = secrets.watch(&ListParams::default(), &resource_version).await?.boxed();
 
-    while let Some(status) = stream.try_next().await? {
-        match status {
-            WatchEvent::Added(s) => {
-                log_info(format!("Added {}, validating...", s.name()));
-                validate_secret(s);
-            },
-            WatchEvent::Modified(s) => {
-                log_info(format!("Modified {}, validating...", s.name()));
-                validate_secret(s)
-            },
-            WatchEvent::Deleted(_s) => {},
-            WatchEvent::Bookmark(_s) => {},
-            WatchEvent::Error(s) => log_err(format!("{}", s)),
+        while let Some(event) = stream.try_next().await? {
+            match event {
+                WatchEvent::Added(s) => {
+                    log_debug(format!("{} added, validating...", s.name()));
+                    validate_secret(s).await;
+                },
+                WatchEvent::Modified(s) => {
+                    log_debug(format!("{} modified, validating...", s.name()));
+                    validate_secret(s).await;
+                },
+                WatchEvent::Deleted(_s) => {},
+                WatchEvent::Bookmark(bm) => {
+                    log_debug(format!("bookmark: {}", bm.metadata.resource_version));
+                    resource_version = bm.metadata.resource_version;
+                },
+                WatchEvent::Error(s) => log_err(format!("{}", s)),
+            }
+        }
+
+        let secret_list = secrets.list(&ListParams::default()).await?;
+        if let Some(rv) = secret_list.metadata.resource_version {
+            log_debug(format!("new res.ver: {}", rv));
+            resource_version = rv;
         }
     }
-
-    Ok(())
 }
 
-fn validate_secret(s: Secret) {
+async fn validate_secret(s: Secret) {
     let secret_name = s.name();
 
     if let Some(data) = s.data {
@@ -69,7 +113,8 @@ fn validate_secret(s: Secret) {
                 let secret_bytes = &data[k];
                 match validate_yaml(secret_bytes) {
                     Err(e) => {
-                        log_err(format!("{}, key {} contains invalid YAML: {}", secret_name, k, e))
+                        log_err(format!("{}, key {} contains invalid YAML: {}", secret_name, k, e));
+                        notify(format!("{}, key {} contains invalid YAML: {}", secret_name, k, e)).await;
                     }
                     Ok(()) => {
                         log_debug(format!("{}, key {} is valid", secret_name, k))
@@ -79,7 +124,8 @@ fn validate_secret(s: Secret) {
                 let secret_bytes = &data[k];
                 match validate_json(secret_bytes) {
                     Err(e) => {
-                        log_err(format!("{}, key {} contains invalid JSON: {}", secret_name, k, e))
+                        log_err(format!("{}, key {} contains invalid JSON: {}", secret_name, k, e));
+                        notify(format!("{}, key {} contains invalid JSON: {}", secret_name, k, e)).await;
                     }
                     Ok(()) => {
                         log_debug(format!("{}, key {} is valid", secret_name, k))
